b2556e8de7f04e73e9d743f36c98b29b4bb48c7f
NovaDiyor <dbdmjkf@gmail.com>
2022-12-06 13:19:41 +0500

    new

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import *
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ['id', 'username', 'first_name', 'last_name', 'is_active']
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_('Extra'), {'fields': ('role', 'number', 'is_waiter', 'salary')}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+
+admin.site.register(Rooms)
+admin.site.register(Client)
+admin.site.register(Category)
+admin.site.register(Food)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- main/models.py
+++ main/models.py
@@ -9,9 +9,18 @@ class User(AbstractUser):
         (3, 'manager'),
         (4, 'cooker'),
         (5, 'call-center'),
-    ))
-    number = models.BigIntegerField()
+    ), null=True, blank=True)
+    number = models.BigIntegerField(null=True, blank=True)
     img = models.ImageField(upload_to='users/', null=True, blank=True)
+    salary = models.IntegerField(default=0, null=True, blank=True)
+    is_waiter = models.BooleanField()
+    
+    def save(self, *args, **kwargs):
+        if self.role == 2:
+            self.is_waiter = True
+        else:
+            self.is_waiter = False
+        super(User, self).save(*args, **kwargs)
 
 
 class Rooms(models.Model):
@@ -24,10 +33,30 @@ class Client(models.Model):
     phone = models.BigIntegerField()
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=210)
+
+
 class Food(models.Model):
     name = models.CharField(max_length=23232)
     price = models.DecimalField(max_digits=50, decimal_places=2)
+    available = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=210)
+    price = models.IntegerField()
     quantity = models.IntegerField()
+    available = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        quantity = int(self.quantity)
+        if quantity <= 0:
+            self.available = False
+        else:
+            self.available = True
+        super(Product, self).save(*args, **kwargs)
 
 
 class Order(models.Model):
