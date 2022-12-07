e35cf139ad9cea4967ee7f0179ecf19d01110e40
NovaDiyor <dbdmjkf@gmail.com>
2022-12-07 08:48:29 +0500

    'new'

--- main/models.py
+++ main/models.py
@@ -74,4 +74,5 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField()
--- main/views.py
+++ main/views.py
@@ -75,7 +75,7 @@ def dashboard_waiter(request):
 def dashboard_manager(request):
     usr = request.user
     if usr.role == 3:
-        return render(request, 'dashboard/dashboard-manager.html')
+        return render(request, 'dashboard/dashboard-manager.html', context)
     else:
         return redirect('dashboard')
 
@@ -102,7 +102,23 @@ def dashboard_call_center(request):
 def dashboard(request):
     usr = request.user
     if usr.role == 1:
-        return render(request, 'dashboard/dashboard.html')
+        client = Client.objects.all()
+        staff = User.objects.all()
+        order = Order.objects.all(done=True)
+        total = 0
+        revenue = 0
+        for i in order:
+            item = OrderItem.objects.get(order=i)
+            total += item.quantity * item.price
+            for x in staff:
+                revenue += total - x.salary
+        context = {
+            'client': client.count(),
+            'staff': staff.count(),
+            'total': total,
+            'revenue': revenue
+        }
+        return render(request, 'dashboard/dashboard.html', context)
     elif usr.role == 2:
         return redirect('dashboard-w')
     elif usr.role == 3:
@@ -155,9 +171,25 @@ def cooker_view(request):
 def waiters_view(request):
     user = request.user
     if user.role == 1:
-        waiters = User.objects.filter(role=2)
+        day = date.today()
+        waiter = User.objects.filter(status=2)
+        month = 0
+        for i in waiter:
+            order = Order.objects.filter(done=True, date__day=day.day, user=i)
+            for x in order:
+                item = OrderItem.objects.get(order=x)
+                for n in item:
+                    if n.product:
+                        i.salary += n.quantity * n.product.price % 8
+                    else:
+                        i.salary += n.quantity * n.food.price % 8
+                    month += i.salary
+                    if i.date__day > day.day:
+                        i.salary = 0
+                    i.save()
         context = {
-            'waiters': waiters
+            'month': month,
+            'waiters': waiter,
         }
         return render(request, 'staff/waiter.html', context)
     elif user.role == 3:
