a1826f46e23e8b73833b0ee11f4a34f2d2223a58
firdavth <firdavskingf@gmail.com>
2022-12-14 11:35:16 +0500

    new

--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from.serializer import *
+from main.models import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+
+
+# class Get_product(APIView):
+#     def get(self,request):
+#         product = Product.objects.filter(available=True)
+#         products = []
+#         for i in product:
+#             data = {
+#
+#             }
+
+
+
