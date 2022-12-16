7d97a91de121d865f5c570f525c84547662ed5b0
NovaDiyor <dbdmjkf@gmail.com>
2022-12-16 16:32:33 +0500

    new

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,20 +1,11 @@
-let order = document.querySelector('.table__order')
-    delivery = document.querySelector('.table__delivery')
-    btn_order = document.getElementById('order_btn')
-    btn_delivery = document.getElementById('delivery_btn')
+let other = document.querySelector('other')
+let input = document.getElementById('.date-input')
+let btn = document.getElementById('btn')
 
-btn_order.addEventListener('click', (e) => {
+btn.addEventListener('click', (e) => {
     e.preventDefault()
-    delivery.style.display = 'none'
-    order.style.display = ''
-    order.style.width = '100%'
+    other.style.display = 'none'
+    input.style.display = 'block'
     console.log('gg')
 })
 
-btn_delivery.addEventListener('click', (e) => {
-    e.preventDefault()
-    delivery.style.display = ''
-    delivery.style.width = '100%'
-    order.style.display = 'none'
-    console.log('gg')
-})
